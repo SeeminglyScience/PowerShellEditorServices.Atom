a91b90d92e47fc6f169c9b85c9dd51463fb1d24a
Patrick M. Meinecke <seeminglyscience@gmail.com>
2018-01-21 22:10:18 -0500

    Fix another appveyor build error

--- build.ps1
+++ build.ps1
@@ -5,7 +5,6 @@ param(
 end {
     & "$PSScriptRoot\tools\AssertRequiredModule.ps1" InvokeBuild 5.0.0 -Force:$Force.IsPresent
     $invokeBuildSplat = @{
-        Task = 'PreRelease'
         File = "$PSScriptRoot/PowerShellEditorServices.Atom.build.ps1"
         Force = $Force.IsPresent
         Configuration = 'Release'
